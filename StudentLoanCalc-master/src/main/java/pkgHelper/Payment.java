0396e9733fdb38e23e0552c5042ff0ebee7b86f8
Andrubes <44404745+Andrubes@users.noreply.github.com>
2019-05-24 22:40:37 -0400

    Update Payment.java

--- StudentLoanCalc-master/src/main/java/pkgHelper/Payment.java
+++ StudentLoanCalc-master/src/main/java/pkgHelper/Payment.java
@@ -8,6 +8,7 @@ import org.apache.poi.ss.formula.functions.*;
 import org.apache.poi.ss.formula.functions.Finance;
 
 public class Payment {
+	
 	private int PaymentID;
 	private Date DueDate;
 	private double IPMT;
@@ -17,32 +18,36 @@ public class Payment {
 	private Loan l;
 	
 	public Payment(int paymentID, Date dueDate, double balance, Loan l) {
+		
 		PaymentID=paymentID;
 		DueDate=dueDate;
 		Balance=balance;
+		
 		this.l=l;
 		this.PPMT=Math.abs(getPPMT());
 		this.TotalPrincipal=this.PPMT +l.getExtraPayment();
 		this.IPMT=(balance)*(l.getIntRate()/12);
 		this.Balance=balance-this.TotalPrincipal;
 	}
+	
 	private double getPPMT() {
-		return Finance.ppmt(l.getIntRate()/12, this.PaymentID, l.getTerm()*12, l.getLoanAmount());
+		return Finance.ppmt(l.getIntRate()/12, this.PaymentID, l.getTerm() * 12, l.getLoanAmount());
 	}
+	
 	public double getBalance() {
 		Balance=Math.round(Balance*100.0)/100.0;
 		return Balance;
 	}
+	
 	public void ZeroBalance() {
 		Balance=0;
 	}
+	
 	public int getPayID() {
 		return PaymentID;
 	}
+	
 	public double getIPMT() {
 		return IPMT;
 	}
 }
-	
-	
-
