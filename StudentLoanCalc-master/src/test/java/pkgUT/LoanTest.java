a0dc5c7bf824dceebc1f8efd6d466cfb8ed9c3b9
Andrubes <44404745+Andrubes@users.noreply.github.com>
2019-05-24 22:41:14 -0400

    Update LoanTest.java

--- StudentLoanCalc-master/src/test/java/pkgUT/LoanTest.java
+++ StudentLoanCalc-master/src/test/java/pkgUT/LoanTest.java
@@ -13,14 +13,15 @@ public class LoanTest {
 	@Test
 	public void LoanTest() {
 		
-		double loanAmount = 100000;
+		double loanAmount = 150000;
 		double interestRate = .07;
-		int term = 20;
+		int term = 25;
 		double extraPayment = 0;
-		Date dueDate = Loan.parseDate("2019-07-04");
+		Date dueDate = Loan.parseDate("2019-09-12");
 		boolean Compound = false;
 		double futureValue = 0;
 		
 		Loan l = new Loan(loanAmount, interestRate, term, extraPayment, dueDate, Compound, futureValue);
 	}
 }
+
