a15e3078b7c451ad3ee2d80e33e32e79531baaaa
Andrubes <44404745+Andrubes@users.noreply.github.com>
2019-05-24 22:42:18 -0400

    Update LoanCalcViewController.java

--- StudentLoanCalc-master/src/main/java/app/controller/LoanCalcViewController.java
+++ StudentLoanCalc-master/src/main/java/app/controller/LoanCalcViewController.java
@@ -25,18 +25,14 @@ public class LoanCalcViewController implements Initializable   {
 	
 	@FXML
 	private TextField LoanAmount;
-
 	@FXML
 	private TextField InterestRate;
 	@FXML
 	private TextField termYear;
-	
 	@FXML
 	private Label lblTotalPayemnts;
-	
 	@FXML
 	private DatePicker PaymentStartDate;
-	
 	@FXML
 	private TextField AdditionalPayment;
 	@FXML
@@ -63,18 +59,23 @@ public class LoanCalcViewController implements Initializable   {
 
 		System.out.println("Amount: " + LoanAmount.getText());
 		double dLoanAmount = Double.parseDouble(LoanAmount.getText());
+
 		System.out.println("Amount: " + dLoanAmount);
 		double interestRate = Double.parseDouble(InterestRate.getText());
+
 		int Term = Integer.parseInt(termYear.getText());
 		
 		LocalDate localDate = PaymentStartDate.getValue();
 		Instant instant = Instant.from(localDate.atStartOfDay(ZoneId.systemDefault()));
+
 		Date date = Date.from(instant);
 		boolean CompoundType=true;
+
 		double futureValue=0;
 		
 		double extraPayment=Double.parseDouble(AdditionalPayment.getText());
 		Loan myLoan= new Loan(dLoanAmount,interestRate,Term,extraPayment,date,CompoundType,futureValue);
+
 		lblTotalPayemnts.setText(myLoan.getTotalPayment());
 		System.out.println(localDate);
 		
