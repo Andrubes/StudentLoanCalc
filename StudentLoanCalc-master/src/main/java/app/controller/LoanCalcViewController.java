2d60bd578ac60359996d53cdedf43aceedb9d279
Andrubes <44404745+Andrubes@users.noreply.github.com>
2019-05-24 22:37:21 -0400

    Add files via upload

---
+++ StudentLoanCalc-master/src/main/java/app/controller/LoanCalcViewController.java
@@ -0,0 +1,86 @@
+package app.controller;
+
+import app.StudentCalc;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import pkgHelper.Loan;
+
+import java.net.URL;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+
+
+public class LoanCalcViewController implements Initializable   {
+
+	private StudentCalc SC = null;
+	
+	@FXML
+	private TextField LoanAmount;
+
+	@FXML
+	private TextField InterestRate;
+	@FXML
+	private TextField termYear;
+	
+	@FXML
+	private Label lblTotalPayemnts;
+	
+	@FXML
+	private DatePicker PaymentStartDate;
+	
+	@FXML
+	private TextField AdditionalPayment;
+	@FXML
+	private Label Totalinterest;
+	
+	
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+	}
+
+	public void setMainApp(StudentCalc sc) {
+		this.SC = sc;
+	}
+	
+	/**
+	 * btnCalcLoan - Fire this event when the button clicks
+	 * 
+	 * @version 1.0
+	 * @param event
+	 */
+	@FXML
+	private void btnCalcLoan(ActionEvent event) {
+
+		System.out.println("Amount: " + LoanAmount.getText());
+		double dLoanAmount = Double.parseDouble(LoanAmount.getText());
+		System.out.println("Amount: " + dLoanAmount);
+		double interestRate = Double.parseDouble(InterestRate.getText());
+		int Term = Integer.parseInt(termYear.getText());
+		
+		LocalDate localDate = PaymentStartDate.getValue();
+		Instant instant = Instant.from(localDate.atStartOfDay(ZoneId.systemDefault()));
+		Date date = Date.from(instant);
+		boolean CompoundType=true;
+		double futureValue=0;
+		
+		double extraPayment=Double.parseDouble(AdditionalPayment.getText());
+		Loan myLoan= new Loan(dLoanAmount,interestRate,Term,extraPayment,date,CompoundType,futureValue);
+		lblTotalPayemnts.setText(myLoan.getTotalPayment());
+		System.out.println(localDate);
+		
+		Totalinterest.setText(myLoan.getTotalInterest());
+	}
+
+}
+
+
---
+++ StudentLoanCalc-master/src/main/java/pkgHelper/Loan.java
@@ -0,0 +1,87 @@
+package pkgHelper;
+
+import java.util.LinkedList;
+import java.text.ParseException;
+import java.util.Date;
+import java.text.SimpleDateFormat;;
+public class Loan {
+	private double LoanAmount;
+	private double IntRate;
+	private int Term;
+	private double ExtraPayment;
+	private Date FirstDueDate;
+	private boolean bCompoundType;
+	private double FutureValue;
+	
+	private LinkedList<Payment> LoanPayments = new LinkedList<Payment>();
+	
+	public Loan(double loanAmount,double InterestRate,int termYear, double extraPayment, Date dueDate, boolean bCompoundType,double futureValue) {
+		super();
+		
+		LoanAmount=loanAmount;
+		IntRate=InterestRate;
+		Term=termYear;
+		ExtraPayment=extraPayment;
+		FirstDueDate= dueDate;
+		this.bCompoundType=bCompoundType;
+		FutureValue=futureValue;
+		
+		double dRollingBalance=this.LoanAmount;
+		int iPaymentNum=0;
+		do {
+			Payment p= new Payment(++iPaymentNum,dueDate,dRollingBalance,this);
+			LoanPayments.add(p);
+			dRollingBalance=p.getBalance();
+			
+			System.out.print("Payment Num ");
+			System.out.print(iPaymentNum);
+			System.out.print(" ");
+			System.out.println(LoanPayments.getLast().getBalance());
+			
+			if(LoanPayments.getLast().getBalance()<=0) {
+				LoanPayments.getLast().ZeroBalance();
+				break;
+			}	
+		}while(true);
+		System.out.println(LoanPayments.size());
+	}
+	public double getLoanAmount() {
+		return LoanAmount;
+	}
+	public double getIntRate() {
+		return IntRate;
+	}
+	public int getTerm() {
+		return Term;
+	}
+	public double getExtraPayment() {
+		return ExtraPayment;
+	}
+	public double getFutureValue() {
+		return FutureValue;
+	}
+	public static Date parseDate(String date) {
+		try {
+			return new SimpleDateFormat("yyy-MM-dd").parse(date);
+		}catch(ParseException e) {
+			return null;
+		}
+	}
+	
+	public String getTotalInterest() {
+		double sum=0;
+		for(Payment p:LoanPayments) {
+			sum+=p.getIPMT();
+		}
+		return ""+(sum);
+	}
+	public String getTotalPayment() {
+		double sum=0;
+		for(Payment p:LoanPayments) {
+			sum+=p.getIPMT();
+		}
+		return ""+(sum+LoanAmount);
+	}
+	
+	
+}
